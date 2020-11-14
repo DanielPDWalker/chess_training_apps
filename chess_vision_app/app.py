4e053e630aac188ae7f2d39dfb62aac6ef1ad337
danielpdwalker <danielpdwalker@gmail.com>
2020-11-14 09:37:04 +0000

    Added main_screen and imported to app.py for use

--- chess_vision_app/app.py
+++ chess_vision_app/app.py
@@ -1,1 +1,15 @@
 import PySimpleGUI as sg
+from screens.main_screen import main_screen
+
+
+layout = main_screen()
+window = sg.Window("Chess Vision Training App", layout)
+app_running = True
+
+while app_running:
+    event, values = window.read()
+    if event == "Close" or event == sg.WIN_CLOSED:
+        break
+    if event == "Start":
+        layout = training_screen()
+        window = sg.Window("Chess Vision Training App", layout)
--- chess_vision_app/screens/main_screen.py
+++ chess_vision_app/screens/main_screen.py
@@ -1,1 +1,13 @@
 import PySimpleGUI as sg
+
+
+def main_screen():
+    main_screen_layout = [
+        [sg.Text("Chess Vision Training App", justification="center",
+                 size=(20, 1), font=("arial", 25))],
+        [sg.Button("Start", font=("arial", 12),
+                   size=(6, 1), pad=(160, 25))],
+        [sg.Button("Close", font=("arial", 12),
+                   size=(6, 1), pad=(160, 10))],
+    ]
+    return main_screen_layout
