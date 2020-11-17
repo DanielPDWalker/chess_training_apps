c100ebd2b5607f435ab8bc136d9b84c83e80479f
danielpdwalker <danielpdwalker@gmail.com>
2020-11-17 16:51:31 +0000

    Added event handling for dark and light buttons, added training loop

--- chess_vision_app/app.py
+++ chess_vision_app/app.py
@@ -1,6 +1,9 @@
 import PySimpleGUI as sg
+
+
 from screens.main_screen import main_screen
-from screens.training_screen import training_screen
+from screens.training_screen import training_screen, training_screen_new_question
+from squares_data import dark, light
 
 
 layout = main_screen()
@@ -12,7 +15,29 @@ while app_running:
     if event == "Close" or event == sg.WIN_CLOSED:
         break
     if event == "Start":
-        layout = training_screen()
-        window = sg.Window("Chess Vision Training App", layout)
-        #training_question_count = 0
-        # while training_question_count < 20:
+        window.close()
+        layout, chosen_square = training_screen()
+        window = sg.Window("Chess Vision Training App", layout, finalize=True)
+
+        training_question_count = 0
+        correct = 0
+        incorrect = 0
+
+        event, values = window.read()
+
+    if event == "Dark" or "Light":
+        while training_question_count < 20:
+            chosen_square_color = event
+            if chosen_square_color in dark:
+                correct_square_color = 'Dark'
+            else:
+                correct_square_color = 'Light'
+            if chosen_square_color == correct_square_color:
+                correct += 1
+            else:
+                incorrect += 1
+            training_question_count += 1
+            chosen_square = training_screen_new_question()
+            window['q'].update(value=chosen_square)
+            event, values = window.read()
+        else:
